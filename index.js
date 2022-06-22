76b385c55f270d8f842881824d14baf4263156b5
Khloe Ramdhan <75541476+khloe-r@users.noreply.github.com>
2022-06-22 03:50:31 +0000

    added extra input fields + send response after modal submission

--- index.js
+++ index.js
@@ -9,19 +9,14 @@ const app = new App({
 });
 
 app.command('/new-request', async ({ ack, body, client, logger }) => {
-  // Acknowledge the command request
   await ack();
 
   try {
-    // Call views.open with the built-in client
     const result = await client.views.open({
-      // Pass a valid trigger_id within 3 seconds of receiving it
       trigger_id: body.trigger_id,
-      // View payload
       view: {
         type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
+        callback_id: 'marketing_request',
         title: {
           type: 'plain_text',
           text: 'Marketing Request Bot'
@@ -32,6 +27,81 @@ app.command('/new-request', async ({ ack, body, client, logger }) => {
             text: {
               type: 'mrkdwn',
               text: 'Welcome! Fill out the form below or tag Khloe or Juan for help!'
+            }
+          },
+          {
+            "type": "section",
+            block_id: 'department',
+            "text": {
+              "type": "mrkdwn",
+              "text": "What do you need help with?"
+            },
+            "accessory": {
+              "type": "static_select",
+              "placeholder": {
+                "type": "plain_text",
+                "text": "Select...",
+                "emoji": true
+              },
+              "options": [
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Graphic (For Social Media)",
+                    "emoji": true
+                  },
+                  "value": "value-0"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Graphic (Not for Posting)",
+                    "emoji": true
+                  },
+                  "value": "value-1"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Video",
+                    "emoji": true
+                  },
+                  "value": "value-2"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Website Update",
+                    "emoji": true
+                  },
+                  "value": "value-3"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Blog/Podcast",
+                    "emoji": true
+                  },
+                  "value": "value-4"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Newsletter Item",
+                    "emoji": true
+                  },
+                  "value": "value-5"
+                },
+                {
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Other",
+                    "emoji": true
+                  },
+                  "value": "value-6"
+                }
+              ],
+              "action_id": "static_select-action"
             }
           },
           {
@@ -46,7 +116,56 @@ app.command('/new-request', async ({ ack, body, client, logger }) => {
               action_id: 'task_input',
               multiline: true
             }
-          }
+          },
+          {
+            type: 'input',
+            block_id: 'info_docs',
+            optional: true,
+            label: {
+              type: 'plain_text',
+              text: 'Link any documents/files with relevant info:'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'info_input',
+              multiline: true
+            }
+          },
+          {
+      			"type": "section",
+            block_id: 'deadline',
+      			"text": {
+      				"type": "mrkdwn",
+      				"text": "When do you need the task done for"
+      			},
+      			"accessory": {
+      				"type": "datepicker",
+      				"initial_date": "2022-06-25",
+      				"placeholder": {
+      					"type": "plain_text",
+      					"text": "Select a date",
+      					"emoji": true
+      				},
+      				"action_id": "datepicker-action"
+      			},
+      		},
+          {
+      			"type": "section",
+            block_id: 'requester',
+      			"text": {
+      				"type": "mrkdwn",
+      				"text": "Who should marketing be in contact with about this request?"
+      			},
+      			"accessory": {
+      				"type": "users_select",
+      				"placeholder": {
+      					"type": "plain_text",
+      					"text": "Select a user",
+      					"emoji": true
+      				},
+      				"action_id": "users_select-action"
+      			}
+      		}
         ],
         submit: {
           type: 'plain_text',
@@ -58,6 +177,77 @@ app.command('/new-request', async ({ ack, body, client, logger }) => {
   catch (error) {
     logger.error(error);
   }
+});
+
+app.view('marketing_request', async ({ ack, view, client, logger }) => {
+  await ack();
+  const info =  view.state.values['info_docs']['info_input'].value;
+  const task = view.state.values['task_required']['task_input'].value;
+  const deadline = view.state.values['deadline']['datepicker-action']['selected_date'];
+  const department = view.state.values['department']['static_select-action']['selected_option']['text']['text'];
+  const requester = view.state.values['requester']['users_select-action']['selected_user'];
+
+  let responsible = '';
+  
+  try {
+    await client.chat.postMessage({
+      channel: 'C03MAUP6G2C',
+      "blocks": [
+    		{
+    			"type": "section",
+    			"text": {
+    				"type": "mrkdwn",
+    				"text": `New ${department} request for ${responsible}`
+    			}
+    		},
+    		{
+    			"type": "section",
+    			"text": {
+    				"type": "mrkdwn",
+    				"text": `*Task:* ${task}\n*Info:* ${info} \n*Deadline:* ${deadline}`
+    			}
+    		},
+    		{
+    			"type": "actions",
+    			"elements": [
+    				{
+    					"type": "button",
+    					"text": {
+    						"type": "plain_text",
+    						"emoji": true,
+    						"text": "In Progress"
+    					},
+    					"value": "click_me_123"
+    				},
+    				{
+    					"type": "button",
+    					"text": {
+    						"type": "plain_text",
+    						"emoji": true,
+    						"text": "Complete"
+    					},
+    					"style": "primary",
+    					"value": "click_me_123"
+    				}
+    			]
+    		},
+        {
+			"type": "context",
+			"elements": [
+				{
+					"type": "plain_text",
+					"text": "Sent from:",
+					"emoji": true
+				}
+			]
+		}
+    	]
+    });
+  }
+  catch (error) {
+    logger.error(error);
+  }
+
 });
 
 (async () => {
